6f728cf0345d73dbd9ac309c83ab54bb7276d68c
Tee22 <traceyOnim22@gmail.com>
2020-12-23 14:27:23 +0300

    added topbar progress indicator

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,15 +14,23 @@ import "../css/app.scss"
 //
 import "phoenix_html"
 import {Socket} from "phoenix"
-import NProgress from "nprogress"
+// import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
+import topbar from "topbar"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+// window.addEventListener("phx:page-loading-start", info => NProgress.start())
+// window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+
+// Show progress bar on live navigation and form submits
+topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
+window.addEventListener("phx:page-loading-start", info => topbar.show())
+window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+
+
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
