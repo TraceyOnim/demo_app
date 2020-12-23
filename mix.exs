20db091961d43ea851d27f7379e796ba7e835404
Tee22 <traceyOnim22@gmail.com>
2020-12-23 13:26:58 +0300

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Surakitabu.MixProject do
     [
       app: :surakitabu,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
