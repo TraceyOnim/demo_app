507755909052d34499d984cf1f9a0e79a525993e
Tee22 <traceyOnim22@gmail.com>
2020-12-26 23:21:34 +0300

    enable creting of post

--- lib/surakitabu_web/live/page_live.ex
+++ lib/surakitabu_web/live/page_live.ex
@@ -1,17 +1,23 @@
 defmodule SurakitabuWeb.PageLive do
   use SurakitabuWeb, :live_view
+  use Phoenix.HTML
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, socket |> assign(post: "")}
   end
 
   def render(assigns) do
     ~L"""
-    <%= f = form_for :post, "#" %>
+    <%= f = form_for :post, "#", [phx_submit: :post] %>
     <label for="post">create post</label>
     <textarea id="post" name="post"  placeholder="what's on your mind?" autofocus></textarea>
     <%= submit "Post", class: "btn" %>
     </form>
+    <p><%= @post %></p>
     """
+  end
+
+  def handle_event("post", %{"post" => post}, socket) do
+    {:noreply, socket |> assign(post: post)}
   end
 end
