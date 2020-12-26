7bfa2a7a954ddd0721c66e718c793b9af764608b
Tee22 <traceyOnim22@gmail.com>
2020-12-26 21:39:49 +0300

    remove test

--- test/surakitabu_web/live/page_live_test.exs
+++ test/surakitabu_web/live/page_live_test.exs
@@ -1,11 +1,5 @@
 defmodule SurakitabuWeb.PageLiveTest do
-  use SurakitabuWeb.ConnCase
+  # use SurakitabuWeb.ConnCase
 
-  import Phoenix.LiveViewTest
-
-  test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
-  end
+  # import Phoenix.LiveViewTest
 end
