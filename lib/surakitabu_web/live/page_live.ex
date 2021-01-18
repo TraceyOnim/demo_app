3e10f7dedd82a83ef7d20c6dac90b80053f2067c
TraceyOnim <traceyOnim22@gmail.com>
2021-01-18 22:32:03 +0300

    modify component to be stateful

--- lib/surakitabu_web/live/page_live.ex
+++ lib/surakitabu_web/live/page_live.ex
@@ -1,24 +1,9 @@
 defmodule SurakitabuWeb.PageLive do
   use SurakitabuWeb, :live_view
-  alias Surakitabu.Post.Account
+
+  alias Surakitabu.Post
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
-  end
-
-  def handle_event("post", params, socket) do
-    case Account.create_post(params) do
-      {:ok, _} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "post created successfully")
-         |> push_redirect(to: "/")}
-
-      {:error, _} ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "Something went wrong, Try Again")
-         |> push_redirect(to: "/")}
-    end
+    {:ok, socket |> assign(posts: Surakitabu.Repo.all(Post))}
   end
 end
--- lib/surakitabu_web/live/post_component.ex
+++ lib/surakitabu_web/live/post_component.ex
@@ -2,15 +2,40 @@ defmodule SurakitabuWeb.PostComponent do
   use Phoenix.LiveComponent
   use Phoenix.HTML
 
+  alias Surakitabu.Post.Account
+
   def render(assigns) do
     ~L"""
     <div>
-    <%= f = form_for :post, "#", [phx_submit: :post] %>
+    <%= f = form_for :post, "#", [phx_submit: :post, phx_target: @myself] %>
     <label for="post">create post</label>
     <textarea id="post" name="post"  placeholder="what's on your mind?" autofocus></textarea>
     <%= submit "Post", class: "btn" %>
     </form>
     </div>
+    <%= unless Enum.empty?(@posts) do %>
+    <div>
+      <%= for post <- @posts do %>
+        <p><%= post.post %></p>
+      <% end %>
+    </div>
+    <% end %>
     """
+  end
+
+  def handle_event("post", params, socket) do
+    {:noreply, socket |> _save_post(params) |> push_redirect(to: "/")}
+  end
+
+  defp _save_post(socket, params) do
+    case Account.create_post(params) do
+      {:ok, _} ->
+        socket
+        |> put_flash(:info, "post created successfully")
+
+      {:error, _} ->
+        socket
+        |> put_flash(:error, "Something went wrong, Try Again")
+    end
   end
 end
